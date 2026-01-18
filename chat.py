3a0b1649519f950d01afa7bf712e9494852a19fd
taufiq-ai <taufiqkhantusar@gmail.com>
2026-01-18 23:20:46 +0600

    Return dummy response for now

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 import sys
 import argparse
 
-from app.orchastrator import chat
+from src.app.orchastrator import chat
 
 parser = argparse.ArgumentParser(prog='cmd based chat for Medicine Agent')
 
@@ -10,9 +10,10 @@ def handle():
         'text', type=str, help='Handle for the Medicine Agent chat session'
     )
     args = parser.parse_args()
-    response = chat(args.text)
-    sys.stdout.write(f"Response: {response}\n\n")
-    return args.text
+    response = f"\n\nHardcoded Dummy Response: {args.text}\n\n"
+    sys.stdout.write(response)
+    return response
+
 
 if __name__=="__main__":
     handle()
